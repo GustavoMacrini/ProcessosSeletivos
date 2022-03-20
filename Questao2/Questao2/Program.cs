6d989dcbd675e61debe0c7944674de68df434162
GustavoMacrini <gustavo.godencio@hotmail.com>
2022-03-19 21:11:48 -0300

    questao2

---
+++ Questao2/Questao2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 0;
+            int b = 1;
+            int c = a + b;
+            bool fim = false;
+
+            try
+            {
+                Console.Write("Informe um número inteiro: ");
+                int num = int.Parse(Console.ReadLine());
+
+                do
+                {
+                    if (num == a || num == b || num == c)
+                    {
+                        Console.WriteLine($"O valor {num} pertence a sequencia!");
+                        fim = true;
+                    }
+
+                    if (c > num)
+                    {
+                        Console.WriteLine($"O valor {num} não pertence a sequencia!");
+                        fim = true;
+                    }
+
+                    a = b;
+                    b = c;
+                    c = a + b;
+
+                } while (fim != true);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Format error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected error: " + ex.Message);
+            }
+
+        }
+    }
+}
